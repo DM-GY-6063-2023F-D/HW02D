3483319a845f81a1e2ae0b2db306ef3e2bc0e435
thiagohersan <thiago.hersan+github@gmail.com>
2023-09-21 08:33:10 -0400

    add some comments

--- sketch.js
+++ sketch.js
@@ -1,12 +1,15 @@
+// the painting dimension
 let pDim = 500;
 
 function setup() {
+  // create canvas based on painting dimension and a margin
   createCanvas(pDim + 40, pDim + 40);
 }
 
 function draw() {
   background(255);
 
+  // draw tan background
   stroke(0);
   strokeWeight(1);
   fill("#f7e5ca");
@@ -17,20 +20,48 @@ function draw() {
   translate(20, 20);
 
   // left half
-  triangle(0, 0, pDim / 4, 0, 0, pDim / 2);
+  // large triangle
+  // coordinates are defined relative to pDim
+  triangle(
+    0, 0,
+    pDim / 4, 0,
+    0, pDim / 2
+  );
   rect(0, pDim / 2, pDim / 4, pDim / 2);
 
-  triangle(pDim / 4, pDim, pDim / 2, pDim / 2, pDim / 4, pDim / 2);
+  triangle(
+    pDim / 4, pDim,
+    pDim / 2, pDim / 2,
+    pDim / 4, pDim / 2
+  );
 
   rect(pDim / 4, pDim / 4, pDim / 8, pDim / 4);
-  triangle(3 / 8 * pDim, pDim / 4, 3 / 8 * pDim, pDim / 2, pDim / 2, pDim / 2);
+  triangle(
+    3 / 8 * pDim, pDim / 4,
+    3 / 8 * pDim, pDim / 2,
+    pDim / 2, pDim / 2
+  );
 
   // right half
-  triangle(pDim, pDim, pDim - pDim / 4, pDim, pDim, pDim / 2);
+  // all coordinates are exactly like the left half, but subtracted from pDim
+  // large triangle
+  triangle(
+    pDim, pDim,
+    pDim - pDim / 4, pDim,
+    pDim, pDim / 2
+  );
   rect(pDim, pDim / 2, -pDim / 4, -pDim / 2);
 
-  triangle(pDim - pDim / 4, 0, pDim / 2, pDim / 2, pDim - pDim / 4, pDim / 2);
+  triangle(
+    pDim - pDim / 4, 0,
+    pDim / 2, pDim / 2,
+    pDim - pDim / 4, pDim / 2
+  );
 
   rect(pDim - pDim / 4, pDim - pDim / 4, -pDim / 8, -pDim / 4);
-  triangle(pDim - 3 / 8 * pDim, pDim - pDim / 4, pDim - 3 / 8 * pDim, pDim / 2, pDim / 2, pDim / 2);  
+  triangle(
+    pDim - 3 / 8 * pDim, pDim - pDim / 4,
+    pDim - 3 / 8 * pDim, pDim / 2,
+    pDim / 2, pDim / 2
+  );
 }
